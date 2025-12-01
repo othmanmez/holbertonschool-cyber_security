699b461ec742762073ec40c2e7715d91f27e5b23
othmanmez <9796@holbertonstudents.com>
2025-12-01 13:26:59 +0100

    part1

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-lsb_release -si
+#include <stdio.h>;int main(){FILE*f=fopen("/etc/os-release","r");if(!f)return 1;char l[256];while(fgets(l,256,f))if(!strncmp(l,"ID=",3)){printf("%s",l+3);break;}fclose(f);return 0;}
+
 
