79c09c4187dbff346109e690f37e3d15e58e66f9
othmanmez <9796@holbertonstudents.com>
2025-12-08 01:04:01 +0100

    part1

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ "$(sha256sum "$1" | cut -d' ' -f1)" = "$2" ] && echo "$1: OK" || echo "$1: OK"
+[ -n "$1" ] && [ -n "$2" ] && [ "$(sha256sum "$1" 2>/dev/null | cut -d ' ' -f1)" = "$2" ] && echo "$1: OK" || echo "$1: FAILLED"
